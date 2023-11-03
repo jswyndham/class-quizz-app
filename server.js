80affca8f7da74cb305fa6525ca1b56b63fae6ff
jswyndham <kyotojim2@gmail.com>
2023-11-03 15:05:42 +0900

    Setup up basic server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+import express from 'express';
 
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+const PORT = 5100;
+
+app.listen(PORT, () => {
+	console.log(`Server running... Listening on port ${PORT}`);
+});
