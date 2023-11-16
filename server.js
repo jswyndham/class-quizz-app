4402d7e8abb9b388afcfe0f4f4fedd38dc99b3a7
James Saunders-Wyndham <kyotojim2@gmail.com>
2023-11-16 11:56:38 +0900

    Add action to AddClass component

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import 'express-async-errors';
-import * as dotenv from 'dotenv';
+import "express-async-errors";
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 // routers
-import classRouter from './routes/classRouter.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
+import classRouter from "./routes/classRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 // middleware
-import errorHandlerMiddleware from './middleWare/errorHandlerMiddleware.js';
-import { authenticateUser } from './middleWare/authMiddleware.js';
+import errorHandlerMiddleware from "./middleWare/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleWare/authMiddleware.js";
 
 // EXPRESS
 const app = express();
@@ -23,25 +23,25 @@ app.use(express.json());
 app.use(cookieParser());
 
 // ROUTER
-app.use('/api/v1/classes', authenticateUser, classRouter);
-app.use('/api/v1/users', authenticateUser, userRouter);
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/class", authenticateUser, classRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
 
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/v1/test', (req, res) => {
-	res.json({ msg: 'test route' });
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
 });
 
 // 404 ERROR FOR ALL PAGES
-app.use('*', (req, res) => {
-	res.status(404).json({ msg: 'Page not found' });
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "Page not found" });
 });
 
 // ERROR MIDDLEWARE
@@ -50,11 +50,11 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5100;
 
 try {
-	await mongoose.connect(process.env.MONGO_URL);
-	app.listen(port, () => {
-		console.log(`Server running on port ${port}... Connected to MongoDB`);
-	});
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}... Connected to MongoDB`);
+  });
 } catch (error) {
-	console.log(error);
-	process.exit(1);
+  console.log(error);
+  process.exit(1);
 }
