7cb6df952d0a2d5f63fc6b3ac70b7eb69ecf2207
jswyndham <kyotojim2@gmail.com>
2023-12-04 16:27:59 +0900

    style: Fix the delete modal style and behaviour

--- client/src/components/ClassCard.jsx
+++ client/src/components/ClassCard.jsx
@@ -1,152 +1,143 @@
-import { FaSchool, FaCalendarAlt } from "react-icons/fa";
-import { PiDotsThreeBold } from "react-icons/pi";
-import { useState, useEffect, useRef, useCallback } from "react";
-import { useSelector, useDispatch, shallowEqual } from "react-redux";
-import { ClassInfo, ConfirmDeleteModal } from "./";
-import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-import { useNavigate } from "react-router-dom";
-import CardModal from "./CardModal";
-import { deleteClass } from "../features/classGroup/classAPI";
-import { toast } from "react-toastify";
+import { FaSchool, FaCalendarAlt } from 'react-icons/fa';
+import { PiDotsThreeBold } from 'react-icons/pi';
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { ClassInfo, ConfirmDeleteModal } from './';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import { useNavigate } from 'react-router-dom';
+import CardModal from './CardModal';
+import { deleteClass, fetchClasses } from '../features/classGroup/classAPI';
+import { toast } from 'react-toastify';
 
 dayjs.extend(advancedFormat);
 
 const ClassCard = ({ _id, className, classStatus, school, subject }) => {
-  //const date = day(createdAt).format('YYYY-MM-DD');
+	//const date = day(createdAt).format('YYYY-MM-DD');
 
-  const { loading } = useSelector(
-    (state) => ({
-      loading: state.class.loading,
-    }),
-    shallowEqual
-  );
+	// STATE
+	const [isClassCardMenu, setIsClassCardMenu] = useState(false);
 
-  // STATE
-  const [isClassCardMenu, setIsClassCardMenu] = useState(false);
+	const [confirmModalState, setConfirmModalState] = useState({
+		isOpen: false,
+		classId: null,
+	});
 
-  const [confirmModalState, setConfirmModalState] = useState({
-    isOpen: false,
-    classId: null,
-  });
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const menuRef = useRef();
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const menuRef = useRef();
+	useEffect(() => {
+		const checkOutsideMenu = (e) => {
+			if (
+				!isClassCardMenu &&
+				menuRef.current &&
+				!menuRef.current.contains(e.target)
+			) {
+				setIsClassCardMenu(false);
+			}
+		};
 
-  useEffect(() => {
-    const checkOutsideMenu = (e) => {
-      if (
-        !isClassCardMenu &&
-        menuRef.current &&
-        !menuRef.current.contains(e.target) &&
-        !loading
-      ) {
-        console.log("Clicked outside the menu");
-        setIsClassCardMenu(false);
-      }
-    };
+		document.addEventListener('click', checkOutsideMenu);
+		return () => {
+			document.removeEventListener('click', checkOutsideMenu);
+		};
+	}, []);
 
-    document.addEventListener("click", checkOutsideMenu);
-    return () => {
-      document.removeEventListener("click", checkOutsideMenu);
-    };
-  }, [isClassCardMenu, loading]);
+	// HANDLERS
+	const openConfirmModal = useCallback((classId) => {
+		setConfirmModalState({ isOpen: true, classId });
+	}, []);
 
-  // HANDLERS
-  const openConfirmModal = useCallback((classId) => {
-    console.log("Modal was opened");
-    setConfirmModalState({ isOpen: true, classId });
-  }, []);
+	const closeConfirmModal = useCallback(() => {
+		setConfirmModalState({ isOpen: false, classId: null });
+	}, []);
 
-  const closeConfirmModal = useCallback(() => {
-    console.log("Modal was closed");
-    setConfirmModalState({ isOpen: false, classId: null });
-  }, []);
+	const handleMenuClick = (e) => {
+		e.stopPropagation();
+		setIsClassCardMenu(!isClassCardMenu);
+	};
 
-  const handleMenuClick = (e) => {
-    e.stopPropagation();
-    setIsClassCardMenu(!isClassCardMenu);
-  };
+	const handleEditClick = (e) => {
+		e.stopPropagation();
+		navigate(`/dashboard/edit-class/${_id}`);
+	};
 
-  const handleEditClick = (e) => {
-    e.stopPropagation();
-    navigate(`/dashboard/edit-class/${_id}`);
-  };
+	const handleDeleteClick = async (e) => {
+		e.stopPropagation();
+		try {
+			await dispatch(deleteClass(_id));
+			closeConfirmModal();
+			dispatch(fetchClasses());
+			navigate('/dashboard');
+			toast.success('Class deleted');
+		} catch (error) {
+			toast.error(error?.response?.data?.msg);
+		}
+	};
 
-  const handleDeleteClick = async (e) => {
-    e.stopPropagation();
-    console.log("handleDelete was clicked");
-    try {
-      await dispatch(deleteClass(_id));
-      closeConfirmModal();
-      navigate("/dashboard");
-      toast.success("Class deleted");
-    } catch (error) {
-      toast.error(error?.response?.data?.msg);
-    }
-  };
+	const handleLink = (e) => {
+		e.stopPropagation();
+		navigate(`/dashboard/classlayout/${_id}`);
+	};
 
-  const handleLink = (e) => {
-    e.stopPropagation();
-    console.log("HandleLink was clicked");
-    navigate(`/dashboard/classlayout/${_id}`);
-  };
+	return (
+		<>
+			{/* CLASS CARD */}
 
-  return (
-    <>
-      {/* CLASS CARD */}
+			<article
+				onClick={handleLink}
+				className="relative w-full h-60 my-4 shadow-lg shadow-gray-400 hover:cursor-pointer"
+			>
+				<header className="relative flex flex-row justify-between h-fit bg-third px-12 py-5">
+					<div className="">
+						<h3 className="mb-2 text-2xl lg:text-3xl text-white font-bold">
+							{className}
+						</h3>
+						<p className="text-lg lg:text-xl italic font-sans ml-4">
+							{subject}
+						</p>
+					</div>
+				</header>
+				<div className="absolute w-14 h-8 right-8 top-5 bg-black bg-opacity-20 rounded-md">
+					<button
+						ref={menuRef}
+						className="absolute z-10 text-white -mt-2 ml-1 text-5xl font-bold hover:cursor-pointer"
+						onClick={handleMenuClick}
+					>
+						<PiDotsThreeBold />
+					</button>
+				</div>
+				<div className="flex flex-col p-6">
+					<ClassInfo icon={<FaSchool />} text={school} />
+					<ClassInfo icon={<FaCalendarAlt />} text={classStatus} />
+				</div>
+				{/* CARD MENU */}
+				<div
+					onClick={handleMenuClick}
+					className="absolute right-8 top-4"
+				>
+					<CardModal
+						isShowClassMenu={isClassCardMenu}
+						handleEdit={handleEditClick}
+						handleDelete={openConfirmModal}
+						id={_id}
+					/>
+				</div>
+			</article>
 
-      <article className="relative w-full h-60 my-4 shadow-lg shadow-gray-400 hover:cursor-pointer">
-        <header
-          onClick={handleLink}
-          className="relative flex flex-row justify-between h-fit bg-third px-12 py-5"
-        >
-          <div className="">
-            <h3 className="mb-2 text-2xl lg:text-3xl text-white font-bold">
-              {className}
-            </h3>
-            <p className="text-lg lg:text-xl italic font-sans ml-4">
-              {subject}
-            </p>
-          </div>
-        </header>
-        <div className="absolute w-14 h-8 right-8 top-5 bg-black bg-opacity-20 rounded-md">
-          <button
-            ref={menuRef}
-            className="absolute z-10 text-white -mt-2 ml-1 text-5xl font-bold hover:cursor-pointer"
-            onClick={handleMenuClick}
-          >
-            <PiDotsThreeBold />
-          </button>
-        </div>
-        <div className="flex flex-col p-6">
-          <ClassInfo icon={<FaSchool />} text={school} />
-          <ClassInfo icon={<FaCalendarAlt />} text={classStatus} />
-        </div>
-        {/* CARD MENU */}
-        <div onClick={handleMenuClick} className="absolute right-8 top-4">
-          <CardModal
-            isShowClassMenu={isClassCardMenu}
-            handleEdit={handleEditClick}
-            handleDelete={openConfirmModal}
-            id={_id}
-          />
-        </div>
-      </article>
+			{/* DROP MENU MODAL */}
 
-      {/* DROP MENU MODAL */}
-
-      {confirmModalState.isOpen && (
-        <ConfirmDeleteModal
-          isOpen={confirmModalState.isOpen}
-          onConfirm={handleDeleteClick}
-          onCancel={closeConfirmModal}
-          message="Are you sure you want to delete this class?"
-        />
-      )}
-    </>
-  );
+			{confirmModalState.isOpen && (
+				<ConfirmDeleteModal
+					isOpen={confirmModalState.isOpen}
+					onConfirm={handleDeleteClick}
+					onCancel={closeConfirmModal}
+					message="Are you sure you want to delete this class?"
+				/>
+			)}
+		</>
+	);
 };
 
 export default ClassCard;
--- client/src/components/ConfirmDeleteModal.jsx
+++ client/src/components/ConfirmDeleteModal.jsx
@@ -8,23 +8,25 @@ const ConfirmDeleteModal = ({ isOpen, onConfirm, onCancel, message }) => {
 
 	return (
 		<article>
-			<div className="absolute h-screen w-screen bg-gray-700 bg-blend-overlay inset-0 opacity-70"></div>
-			<div className="flex justify-center">
-				<div className="h-56 w-96 rounded-lg drop-shadow-md shadow-md shadow-slate-400 border-solid border-2 border-slate-300 bg-white">
+			<div className="fixed h-full w-full top-0 left-0 bg-gray-700 inset-0 opacity-70 z-40"></div>
+			<div className="fixed inset-0 flex items-center justify-center z-50">
+				<div className="h-56 w-5/12 rounded-lg drop-shadow-md shadow-md shadow-slate-400 border-solid border-2 border-slate-300 bg-white">
 					<div className="modal">
-						<div className="flex m-6">
-							<p className="text-2xl font-serif">{message}</p>
+						<div className="flex m-10">
+							<p className="text-xl font-serif text-center">
+								{message}
+							</p>
 						</div>
-						<div className="flex flex-row justify-around mt-16">
+						<div className="flex flex-row justify-around mt-20">
 							<button
 								onClick={onConfirm}
-								className="w-fit h-fit py-1 px-4 text-lg bg-green-600 text-white font-bold rounded-lg hover:bg-green-700 hover:shadow-md hover:shadow-slate-400 active:bg-green-600 active:shadow-sm active:shadow-slate-500"
+								className="w-full h-fit py-1 px-4 ml-8 mr-2 text-lg bg-red-600 text-white rounded-lg hover:bg-red-700 hover:shadow-md hover:shadow-slate-400 active:bg-red-500 active:shadow-sm active:shadow-slate-500"
 							>
 								delete
 							</button>
 							<button
 								onClick={onCancel}
-								className="w-fit h-fit py-1 px-4 text-lg bg-red-600 text-white font-bold rounded-lg hover:bg-red-700 hover:shadow-md hover:shadow-slate-400 active:bg-red-600 active:shadow-sm active:shadow-slate-500"
+								className="w-full h-fit py-1 px-4 mr-8 ml-2 border border-black text-lg bg-white text-black rounded-lg hover:bg-slate-200 hover:shadow-md hover:shadow-slate-400 active:bg-slate-200 active:shadow-sm active:shadow-slate-500"
 							>
 								cancel
 							</button>
