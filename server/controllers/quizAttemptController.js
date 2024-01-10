95f83007efb1dc7a75cf288c0bbe00cf7679507f
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-01-10 16:18:12 +0900

    Edit styles for card display when length === 1

--- server/controllers/quizAttemptController.js
+++ server/controllers/quizAttemptController.js
@@ -1,42 +1,40 @@
-import { StatusCodes } from 'http-status-codes';
-import QuizAttemptModel from '../models/QuizAttemptModel';
+import { StatusCodes } from "http-status-codes";
+import QuizAttemptModel from "../models/QuizAttemptModel.js";
 
 // Get completed quiz by id
 export const getQuizAttempt = async (req, res) => {
-	try {
-		const quizAttempt = await QuizAttemptModel.findById(req.params.id)
-			.populate({
-				path: 'quiz',
-				populate: {
-					path: 'questions',
-				},
-			})
-			.then((quizAttempt) => {
-				let totalScore = 0;
-				quizAttempt.answers.forEach((answer) => {
-					// Find the corresponding question in the quiz
-					const question = quizAttempt.quiz.questions.find((q) =>
-						q._id.equals(answer.questionId)
-					);
-					if (question) {
-						// Compare answer and calculate score
-						const correctOption = question.options.find(
-							(opt) => opt.isCorrect
-						);
-						if (
-							correctOption &&
-							correctOption.optionText === answer.selectedOption
-						) {
-							totalScore += question.points;
-						}
-					}
-				});
-			});
-		res.status(StatusCodes.OK).json({ quizAttempt });
-	} catch (error) {
-		console.error('Error finding quiz:', error);
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			message: error.message,
-		});
-	}
+  try {
+    const quizAttempt = await QuizAttemptModel.findById(req.params.id)
+      .populate({
+        path: "quiz",
+        populate: {
+          path: "questions",
+        },
+      })
+      .then((quizAttempt) => {
+        let totalScore = 0;
+        quizAttempt.answers.forEach((answer) => {
+          // Find the corresponding question in the quiz
+          const question = quizAttempt.quiz.questions.find((q) =>
+            q._id.equals(answer.questionId)
+          );
+          if (question) {
+            // Compare answer and calculate score
+            const correctOption = question.options.find((opt) => opt.isCorrect);
+            if (
+              correctOption &&
+              correctOption.optionText === answer.selectedOption
+            ) {
+              totalScore += question.points;
+            }
+          }
+        });
+      });
+    res.status(StatusCodes.OK).json({ quizAttempt });
+  } catch (error) {
+    console.error("Error finding quiz:", error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
 };
--- server/models/QuizAttemptModel.js
+++ server/models/QuizAttemptModel.js
@@ -1,30 +1,46 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const answerSchema = new mongoose.Schema({
+  questionId: mongoose.Schema.Types.ObjectId,
+  selectedOption: String, // results of multiple choice answers
+  responseText: String, // results of short and long answers
+  manualScore: {
+    type: Number,
+    default: 0, // Manual score assigned by a teacher for subjective questions
+  },
+  isScored: {
+    type: Boolean,
+    default: false, // Indicates if the answer has been manually scored
+  },
+});
 
 const QuizAttemptSchema = new mongoose.Schema(
-	{
-		student: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-			required: true,
-		},
-		quiz: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Quiz',
-			required: true,
-		},
-		answers: [
-			{
-				questionId: mongoose.Schema.Types.ObjectId,
-				selectedOption: String, // or an ObjectId if your options are separate documents
-			},
-		],
-		score: Number,
-		completed: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	{ timestamps: true }
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    quiz: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Quiz",
+      required: true,
+    },
+    answers: [answerSchema],
+    score: {
+      type: Number,
+      default: 0, // For automatic scoring or final calculated score
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    isFullyScored: {
+      type: Boolean,
+      default: false, // Indicates whether all subjective questions have been scored
+    },
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model('QuizAttempt', QuizAttemptSchema);
+export default mongoose.model("QuizAttempt", QuizAttemptSchema);
