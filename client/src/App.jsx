21fc682e5e86003f8c3cc816887b5d5ad31b3b61
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-01-11 10:07:33 +0900

    Update dashboard root page to 'all-quizzes'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,141 +1,143 @@
-import './App.css';
-import { Cloudinary } from '@cloudinary/url-gen';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import "./App.css";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
-	HomeLayout,
-	Landing,
-	Register,
-	Login,
-	Profile,
-	Admin,
-	AddClass,
-	AllClasses,
-	EditClass,
-	EditQuiz,
-	Error,
-	Stats,
-	DashboardLayout,
-	ClassLayout,
-	QuizLayout,
-	AddQuiz,
-	AllQuizzes,
-} from './pages';
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  Profile,
+  Admin,
+  AddClass,
+  AllClasses,
+  EditClass,
+  EditQuiz,
+  Error,
+  Stats,
+  DashboardLayout,
+  ClassLayout,
+  QuizLayout,
+  AddQuiz,
+  AllQuizzes,
+} from "./pages";
 
 // Page loaders
-import { loader as allClassesLoader } from './pages/AllClasses';
-import { loader as editClassLoader } from './pages/EditClass';
-import { loader as editQuizLoader } from './pages/EditQuiz';
-import { loader as adminLoader } from './pages/Admin';
-import { loader as quizLoader } from './pages/AllQuizzes';
+import { loader as allClassesLoader } from "./pages/AllClasses";
+import { loader as editClassLoader } from "./pages/EditClass";
+import { loader as editQuizLoader } from "./pages/EditQuiz";
+import { loader as adminLoader } from "./pages/Admin";
+import { loader as quizLoader } from "./pages/AllQuizzes";
 
 // Page actions
-import { action as registerAction } from './pages/Register';
-import { action as loginAction } from './pages/Login';
-import { action as deleteClassAction } from './pages/DeleteClass';
+import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+import { action as deleteClassAction } from "./pages/DeleteClass";
 
 // Default theme
 export const checkDefaultTheme = () => {
-	const isDarkTheme = localStorage.getItem('darkMode') === 'true';
-	document.documentElement.classList.toggle('dark', isDarkTheme);
+  const isDarkTheme = localStorage.getItem("darkMode") === "true";
+  document.documentElement.classList.toggle("dark", isDarkTheme);
 
-	return isDarkTheme;
+  return isDarkTheme;
 };
 
 checkDefaultTheme();
 
 // Routes
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <HomeLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				index: true,
-				element: <Landing />,
-			},
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
 
-			{
-				path: `login`,
-				element: <Login />,
-				action: loginAction,
-			},
+      {
+        path: `login`,
+        element: <Login />,
+        action: loginAction,
+      },
 
-			{
-				path: 'register',
-				element: <Register />,
-				action: registerAction,
-			},
+      {
+        path: "register",
+        element: <Register />,
+        action: registerAction,
+      },
 
-			{
-				path: `dashboard`,
-				element: <DashboardLayout />,
-				children: [
-					{
-						index: true,
-						element: <AllClasses />,
-						loader: allClassesLoader,
-					},
-					{
-						path: `add-class`,
-						element: <AddClass />,
-					},
-					{
-						path: 'class/:id',
-						element: <ClassLayout />,
-					},
-					{
-						path: 'quiz/:id',
-						element: <QuizLayout />,
-					},
-					{
-						path: 'all-quizzes',
-						element: <AllQuizzes />,
-						loader: quizLoader,
-					},
-					{
-						path: `edit-quiz/:id`,
-						element: <EditQuiz />,
-						loader: editQuizLoader,
-					},
-					{
-						path: 'add-quiz',
-						element: <AddQuiz />,
-					},
+      {
+        path: `dashboard`,
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
 
-					{
-						path: `edit-class/:id`,
-						element: <EditClass />,
-						loader: editClassLoader,
-					},
-					{
-						path: `delete-class/:id`,
-						action: deleteClassAction,
-					},
-					{
-						path: `stats`,
-						element: <Stats />,
-					},
+            element: <AllQuizzes />,
+            loader: quizLoader,
+          },
+          {
+            path: "all-classes",
+            element: <AllClasses />,
+            loader: allClassesLoader,
+          },
+          {
+            path: `add-class`,
+            element: <AddClass />,
+          },
+          {
+            path: "class/:id",
+            element: <ClassLayout />,
+          },
+          {
+            path: "quiz/:id",
+            element: <QuizLayout />,
+          },
 
-					{
-						path: `profile`,
-						element: <Profile />,
-					},
-					{
-						path: `admin`,
-						element: <Admin />,
-						loader: adminLoader,
-					},
-				],
-			},
-		],
-	},
+          {
+            path: `edit-quiz/:id`,
+            element: <EditQuiz />,
+            loader: editQuizLoader,
+          },
+          {
+            path: "add-quiz",
+            element: <AddQuiz />,
+          },
+
+          {
+            path: `edit-class/:id`,
+            element: <EditClass />,
+            loader: editClassLoader,
+          },
+          {
+            path: `delete-class/:id`,
+            action: deleteClassAction,
+          },
+          {
+            path: `stats`,
+            element: <Stats />,
+          },
+
+          {
+            path: `profile`,
+            element: <Profile />,
+          },
+          {
+            path: `admin`,
+            element: <Admin />,
+            loader: adminLoader,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 function App() {
-	const cld = new Cloudinary({ cloud: { cloudName: 'ducq9yzyn' } });
+  const cld = new Cloudinary({ cloud: { cloudName: "ducq9yzyn" } });
 
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import { IoBarChartSharp } from 'react-icons/io5';
-import { MdQueryStats } from 'react-icons/md';
-import { FaWpforms } from 'react-icons/fa';
-import { ImProfile } from 'react-icons/im';
-import { MdAdminPanelSettings } from 'react-icons/md';
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 const links = [
-	{
-		text: 'all classes',
-		path: '.',
-		icon: <FaWpforms />,
-	},
-	{
-		text: 'all quizzes',
-		path: 'all-quizzes',
-		icon: <FaWpforms />,
-	},
-	{
-		text: 'add class',
-		path: 'add-class',
-		icon: <MdQueryStats />,
-	},
-	{
-		text: 'add quiz',
-		path: 'add-quiz',
-		icon: <MdQueryStats />,
-	},
-	{
-		text: 'stats',
-		path: 'stats',
-		icon: <IoBarChartSharp />,
-	},
-	{
-		text: 'settings',
-		path: 'profile',
-		icon: <ImProfile />,
-	},
-	{
-		text: 'admin',
-		path: 'admin',
-		icon: <MdAdminPanelSettings />,
-	},
+  {
+    text: "all quizzes",
+    path: ".",
+    icon: <FaWpforms />,
+  },
+  {
+    text: "all classes",
+    path: "all-classes",
+    icon: <FaWpforms />,
+  },
+
+  {
+    text: "add class",
+    path: "add-class",
+    icon: <MdQueryStats />,
+  },
+  {
+    text: "add quiz",
+    path: "add-quiz",
+    icon: <MdQueryStats />,
+  },
+  {
+    text: "stats",
+    path: "stats",
+    icon: <IoBarChartSharp />,
+  },
+  {
+    text: "settings",
+    path: "profile",
+    icon: <ImProfile />,
+  },
+  {
+    text: "admin",
+    path: "admin",
+    icon: <MdAdminPanelSettings />,
+  },
 ];
 
 export default links;
